885cf340c02ec8820a8b1ff414fc099b21bd47ad
Christophe Marchal <toff63@gmail.com>
2013-07-06 01:47:07 -0300

    moved blog actions from application to blog controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,6 +9,9 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
   
+}
+object Blog extends Controller {
+  
   def post(id: Integer) = Action{
     Ok("This is a fake post with id: " + id)
   }
